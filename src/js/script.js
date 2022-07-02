91d5cf2c15bf4c9be3b1002ffb4443455f79ea84
Eurus <eurus0601@gmail.com>
2022-07-02 20:38:28 +0800

    revision

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,43 @@
-const inputField = document.querySelector("[data-input]");
 const form = document.querySelector("[data-form]");
-const submitBtn = document.querySelector("[data-btn]");
+const emailAddress = document.querySelector("[data-input]");
 const errorIcon = document.querySelector(".err-icon");
 
-submitBtn.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checInputs();
-});
-
-function checInputs() {
-  // const inputFieldValue = inputField.value.trim();
-  const regex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  if (inputField === "" || !regex.test(inputField)) {
-    form.classList.add("error");
-  } else {
-    form.submit();
-  }
 
   errorIcon.classList.remove("anim");
   void errorIcon.offsetWidth;
   errorIcon.classList.add("anim");
+});
+
+function checInputs() {
+  const emailAddressValue = emailAddress.value.trim();
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (emailAddressValue === "" || !re.test(emailAddressValue)) {
+    inputError(emailAddress, "Please provide a valid email");
+    inputError(emailAddress, "Please enter an email");
+  } else {
+    inputSuccess(emailAddress);
+  }
 }
 
-// function inputError(input, message) {
-//   const formControl = input.parentElement;
-//   formControl.className = "featured-panel__form-container error";
+function inputError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "featured-panel__form-container error";
 
-//   const err = formControl.querySelector(".err-message");
-//   err.innerText = message;
-// }
+  const err = formControl.querySelector(".err-message");
+  err.innerText = message;
+}
 
-// function inputSuccess(input) {
-//   const formControl = input.parentElement;
-//   formControl.className = "featured-panel__form-container success";
-// }
+function inputSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "featured-panel__form-container success";
+}
 
-inputField.addEventListener("invalid", (e) => {
+emailAddress.addEventListener("invalid", (e) => {
   e.preventDefault();
 });
